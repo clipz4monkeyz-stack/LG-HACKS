4df40acb580b0ce60c0aa5a67d282f93dd44ec10
Anya Li <anyaliluo@gmail.com>
2025-09-24 23:34:55 -0700

    Initial Commit

---
+++ ALai/ALaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALaiApp.swift
+//  ALai
+//
+//  Created by Anwen Li on 9/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ALaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
